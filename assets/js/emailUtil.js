ad5a8bf3bcb178fb046fac38c501c628da18a2c4
Sharmaine Bajala <95149755+sbajala@users.noreply.github.com>
2023-02-08 12:28:01 -0500

    Add files via upload

---
+++ assets/js/emailUtil.js
@@ -0,0 +1,9 @@
+function copyEmail() {
+    navigator.clipboard.writeText("sj.bajala@gmail.com");
+    
+  }
+
+  function outFunc() {
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copy to clipboard";
+  }
